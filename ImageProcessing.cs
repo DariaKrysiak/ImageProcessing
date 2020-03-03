5fef15792634695d5f4f096f1f1ea69c87a5c9e4
DariaKrysiak <daria.krysiak@gmail.com>
2020-03-03 22:08:04 +0100

    Add methods open and save image

---
+++ ImageProcessing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ImageProcessing
+{
+    public class ImageProcessing
+    {
+        public Image OpenImage(string filePath)
+        {
+            return Image.FromFile(filePath);
+        }
+
+        public void SaveImage(Image img, string newFileName)
+        {
+            img.Save(newFileName);
+        }
+    }
+}
