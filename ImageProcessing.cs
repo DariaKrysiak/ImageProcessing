9a5f7f97c4546eb3eb0ce421bf85b416cc5b57f4
DariaKrysiak <daria.krysiak@gmail.com>
2020-03-04 02:06:28 +0100

    Add ToMainColors method

--- ImageProcessing.cs
+++ ImageProcessing.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 
-namespace ImageProcessing
+namespace ImageProcessingLibrary
 {
     public class ImageProcessing
     {
@@ -14,9 +14,44 @@ namespace ImageProcessing
             return Image.FromFile(filePath);
         }
 
-        public void SaveImage(Image img, string newFileName)
+        public Image ToMainColors(Image image)
         {
-            img.Save(newFileName);
+            Bitmap bitmap = new Bitmap(image);
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    Color pixelColor = bitmap.GetPixel(x, y);
+
+                    int alpha = pixelColor.A;
+                    int red = pixelColor.R;
+                    int green = pixelColor.G;
+                    int blue = pixelColor.B;
+
+                    Color newPixelColor = Color.FromArgb(alpha, 0, 0, 0);
+                    if (red >= green && red >= blue)
+                    {
+                        newPixelColor = Color.FromArgb(alpha, 255, 0, 0);
+                    }
+                    else if (green > red && green > blue)
+                    {
+                        newPixelColor = Color.FromArgb(alpha, 0, 255, 0);
+                    }
+                    else if (blue > red && blue >= green)
+                    {
+                        newPixelColor = Color.FromArgb(alpha, 0, 0, 255);
+                    }
+
+                    bitmap.SetPixel(x, y, newPixelColor);
+                }
+            }
+
+            return (Image)bitmap;
+        }
+
+        public void SaveImage(Image image, string newFilePath)
+        {
+            image.Save(newFilePath);
         }
     }
 }
