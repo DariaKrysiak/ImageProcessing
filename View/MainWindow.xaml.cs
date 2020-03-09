7902fd86217dd0ef4a75fe4ec459d0e6762e902a
DariaKrysiak <daria.krysiak@gmail.com>
2020-03-09 23:06:31 +0100

    Separate ViewModel and View

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -4,20 +4,19 @@ using System.Windows.Media.Imaging;
 using Microsoft.Win32;
 using System.Drawing;
 using ImageProcessingLibrary;
+using Image_Processing_application.ViewModel;
 
 namespace Image_Processing_application.View
 {
     public partial class MainWindow : Window
     {
-        private string fileName;
-
         public MainWindow()
         {
-            InitializeComponent();
-            durationTime.Text = "00:00:00.0000";
+            InitializeComponent();         
+            DataContext = new MainWindowViewModel(CreateDialog());
         }
 
-        private void Load_Button_Click(object sender, RoutedEventArgs e)
+        private OpenFileDialog CreateDialog()
         {
             OpenFileDialog dialog = new OpenFileDialog();
             dialog.Title = "Select a picture";
@@ -25,40 +24,7 @@ namespace Image_Processing_application.View
               "JPG (*.jpg)|*.jpg" +
               "PNG (*.png)|*.png" +
               "BMP (*.bmp)|*.bmp";
-            if (dialog.ShowDialog() == true)
-            {
-                originalImage.Source = new BitmapImage(new Uri(dialog.FileName));
-                fileName = dialog.FileName;
-            }
-        }
-
-        private void Convert_Button_Click(object sender, RoutedEventArgs e)
-        {
-            string fileNameForCovertedImage = PrepareFileNameForConvertedImage();
-            ProcessImage(fileNameForCovertedImage);
-            convertedImage.Source = new BitmapImage(new Uri(fileNameForCovertedImage));
-        }
-
-        private void ProcessImage(string fileNameForCovertedImage)
-        {
-            ImageProcessing processing = new ImageProcessing();
-            Image image = processing.OpenImage(fileName);
-            DateTime startTime = DateTime.Now;
-            Image convertedImage = processing.ToMainColors(image);
-            DateTime endTime = DateTime.Now;
-            processing.SaveImage(convertedImage, fileNameForCovertedImage);
-            DisplayDurationTime(startTime, endTime);
-        }
-
-        private string PrepareFileNameForConvertedImage()
-        {
-            int ExtensionIndex = fileName.LastIndexOf(".");
-            return fileName.Substring(0, ExtensionIndex) + "_converted" + fileName.Substring(ExtensionIndex, fileName.Length - ExtensionIndex);
-        }
-
-        private void DisplayDurationTime(DateTime startTime, DateTime endTime)
-        {
-            durationTime.Text = string.Format("{0:hh}:{0:mm}:{0:ss}.{0:ffff}", (endTime - startTime));
+            return dialog;
         }
     }
 }
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using ImageProcessingLibrary;
+using Microsoft.Win32;
+
+namespace Image_Processing_application.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private OpenFileDialog dialog;
+        private BitmapImage originalImage;
+        private BitmapImage convertedImage;
+        private string durationTime;
+        private string fileName;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand LoadImage
+        {
+            get { return new RelayCommand(LoadImageAction); }
+        }
+
+        public ICommand ConvertImage
+        {
+            get { return new RelayCommand(ConvertImageAction); }
+        }
+
+        public BitmapImage OriginalImage
+        {
+            get { return this.originalImage; }
+            set
+            {
+                this.originalImage = value;
+                this.OnPropertyChanged(nameof(this.OriginalImage));
+            }
+        }
+
+        public BitmapImage ConvertedImage
+        {
+            get { return this.convertedImage; }
+            set
+            {
+                this.convertedImage = value;
+                this.OnPropertyChanged(nameof(this.ConvertedImage));
+            }
+        }
+
+        public string DurationTime
+        {
+            get { return this.durationTime; }
+            set
+            {
+                this.durationTime = value;
+                this.OnPropertyChanged(nameof(this.DurationTime));
+            }
+        }
+
+        public MainWindowViewModel(OpenFileDialog dialog)
+        {
+            this.DurationTime = "00:00:00.0000";
+            this.dialog = dialog;
+        }
+
+        private void LoadImageAction()
+        {
+            if (dialog.ShowDialog() == true)
+            {
+                this.OriginalImage = new BitmapImage(new Uri(dialog.FileName));
+                this.fileName = dialog.FileName;
+            }
+        }
+
+        private void ConvertImageAction()
+        {
+            string fileNameForCovertedImage = PrepareFileNameForConvertedImage();
+            ProcessImage(fileNameForCovertedImage);
+            this.ConvertedImage = new BitmapImage(new Uri(fileNameForCovertedImage));
+        }
+
+        private string PrepareFileNameForConvertedImage()
+        {
+            int ExtensionIndex = this.fileName.LastIndexOf(".");
+            return this.fileName.Substring(0, ExtensionIndex) + "_converted" + this.fileName.Substring(ExtensionIndex, this.fileName.Length - ExtensionIndex);
+        }
+
+        private void ProcessImage(string fileNameForCovertedImage)
+        {
+            ImageProcessing processing = new ImageProcessing();
+            Image image = processing.OpenImage(this.fileName);
+            DateTime startTime = DateTime.Now;
+            Image convertedImage = processing.ToMainColors(image);
+            DateTime endTime = DateTime.Now;
+            processing.SaveImage(convertedImage, fileNameForCovertedImage);
+            DisplayDurationTime(startTime, endTime);
+        }
+
+        private void DisplayDurationTime(DateTime startTime, DateTime endTime)
+        {
+            this.DurationTime = string.Format("{0:hh}:{0:mm}:{0:ss}.{0:ffff}", (endTime - startTime));
+        }
+
+        public void OnPropertyChanged(string property)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
