b6a41b9ea2b51c7d789117a93d97dc43f3f9a497
DariaKrysiak <daria.krysiak@gmail.com>
2020-03-11 21:21:53 +0100

    Add tests got ProcessImage

---
+++ ImageProcessingApplicationTests/Model/ImageProcessingModelTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImageProcessingApplication.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcessingApplication.Model.Tests
+{
+    [TestClass()]
+    public class ImageProcessingModelTests
+    {
+        private ImageProcessingModel imageProcessing;
+        private Dictionary<string, string> fileNames;
+
+        [TestInitialize()]
+        public void Setup()
+        {
+            imageProcessing = new ImageProcessingModel();
+            fileNames = new Dictionary<string, string>()
+            {
+                { "test.jpg", "test_converted.jpg" },
+                { "test.png", "test_converted.png" },
+                { "test.bmp", "test_converted.bmp" }
+            };
+        }
+
+        [TestMethod()]
+        [DeploymentItem(@"test_images")]
+        public void ProcessImageTest()
+        {
+            foreach (KeyValuePair<string, string> fileName in fileNames) 
+            {
+                string convertedFileName = imageProcessing.ProcessImage(fileName.Key);
+                
+                Assert.AreEqual(fileName.Value, convertedFileName, false);
+            }
+        }
+
+        [TestMethod()]
+        [DeploymentItem(@"test_images")]
+        public void ProcessImageDurationTimeSpanTest()
+        {
+            imageProcessing.ProcessImage(fileNames.First().Key);
+
+            Assert.IsTrue(imageProcessing.durationTimeSpan.TotalMilliseconds > 0);
+        }
+    }
+}
