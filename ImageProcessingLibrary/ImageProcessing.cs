98a4f6802d9dfda2734ee8258781cd8c1dc62eab
DariaKrysiak <daria.krysiak@gmail.com>
2020-04-07 01:30:11 +0200

    Add Parallel.For instead of Task.Run

--- ImageProcessingLibrary/ImageProcessing.cs
+++ ImageProcessingLibrary/ImageProcessing.cs
@@ -25,6 +25,33 @@ namespace ImageProcessingLibrary
             return bitmap;
         }
 
+        public Image ToMainColorsAsync(Image image)
+        {
+            Bitmap bitmap = new Bitmap(image);
+            int width = bitmap.Width;
+            int height = bitmap.Height;
+
+            RectangleF cloneRect = new RectangleF(0, 0, width, height);
+            System.Drawing.Imaging.PixelFormat format = bitmap.PixelFormat;
+            Bitmap cloneBitmap = bitmap.Clone(cloneRect, format);
+
+            for (int x = 0; x < width; x++)
+            {
+                Parallel.For(0, height, y =>
+                {
+                    Color pixelColor = cloneBitmap.GetPixel(x, y);
+                    Color newPixelColor = ToMainPixelColor(pixelColor);
+                    bitmap.SetPixel(x, y, newPixelColor);
+                });
+            }
+            return bitmap;
+        }
+
+        public void SaveImage(Image image, string newFilePath)
+        {
+            image.Save(newFilePath);
+        }
+
         private Color ToMainPixelColor(Color pixelColor)
         {
             int alpha = pixelColor.A;
@@ -46,61 +73,6 @@ namespace ImageProcessingLibrary
                 newPixelColor = Color.FromArgb(alpha, 0, 0, 255);
             }
             return newPixelColor;
-        }
-
-        public async Task<Image> ToMainColorsAsync(Image image)
-        {
-            Bitmap bitmap = new Bitmap(image);
-            Task<Color[]>[] tasks = new Task<Color[]>[bitmap.Width];
-
-            for (int x = 0; x < bitmap.Width; x++)
-            {
-                tasks[x] = Task.Run(() =>
-                {
-                    Color[] row = new Color[bitmap.Height];
-                    for (int y = 0; y < bitmap.Height; y++)
-                    {
-                        Color pixelColor = bitmap.GetPixel(x, y);
-                        int alpha = pixelColor.A;
-                        int red = pixelColor.R;
-                        int green = pixelColor.G;
-                        int blue = pixelColor.B;
-
-                        Color newPixelColor = Color.FromArgb(alpha, 0, 0, 0);
-                        if (red >= green && red >= blue)
-                        {
-                            newPixelColor = Color.FromArgb(alpha, 255, 0, 0);
-                        }
-                        else if (green > red && green > blue)
-                        {
-                            newPixelColor = Color.FromArgb(alpha, 0, 255, 0);
-                        }
-                        else if (blue > red && blue >= green)
-                        {
-                            newPixelColor = Color.FromArgb(alpha, 0, 0, 255);
-                        }
-
-                        row[y] = newPixelColor;
-                    }
-
-                    return row;
-                });
-            }
-            Task.WaitAll(tasks);
-
-            for (int x = 0; x < bitmap.Width; x++)
-            {
-                for (int y = 0; y < bitmap.Height; y++)
-                {
-                    bitmap.SetPixel(x, y, tasks[x].Result[y]);
-                }
-            }
-            return bitmap;
-        }
-
-        public void SaveImage(Image image, string newFilePath)
-        {
-            image.Save(newFilePath);
         }
     }
 }
